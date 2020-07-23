5e55f4dc2d755057b31fae2d1ead6d5e1cd17133
5Wymiar <mikolaj.fiedor123@gmail.com>
2020-07-23 11:36:04 +0200

    DOM, downoloads elements and creates ele.

---
+++ main.js
@@ -0,0 +1,9 @@
+const test = document.querySelector('p');
+console.log(test);
+
+const move = document.querySelector('span');
+console.log(move);
+
+const newSection = document.createElement('section');
+newSection.textContent = 'Siema albo sieniema'
+document.body.appendChild(newSection);
